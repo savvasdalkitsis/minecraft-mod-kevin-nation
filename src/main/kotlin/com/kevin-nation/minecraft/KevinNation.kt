c07176331416f8f88ac11d7936d07a82bb46979f
Savvas Dalkitsis <kurosavvas@gmail.com>
2021-03-14 18:28:12 +0000

    Only running client code on the client

--- src/main/kotlin/com/kevin-nation/minecraft/KevinNation.kt
+++ src/main/kotlin/com/kevin-nation/minecraft/KevinNation.kt
@@ -2,6 +2,8 @@ package com.`kevin-nation`.minecraft
 
 import com.kevin.KevinNationMenuScreen
 import net.minecraft.client.gui.screen.MainMenuScreen
+import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.api.distmarker.OnlyIn
 import net.minecraftforge.client.event.GuiOpenEvent
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.eventbus.api.SubscribeEvent
@@ -15,6 +17,7 @@ class KevinNation {
     }
 
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     fun setMainMenuScreen(e: GuiOpenEvent) {
         if (e.gui is MainMenuScreen) {
             e.gui = KevinNationMenuScreen()
